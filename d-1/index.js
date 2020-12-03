fc1fbe90d83f0cda5cb9a960ff4c28fe77d568e2
Emily Harber <emily.a.harber@gmail.com>
2020-12-02 21:58:00 -0500

    add second half instructions and clean up day 2

--- d-1/index.js
+++ d-1/index.js
@@ -36,6 +36,14 @@ function findTwo2020(arr) {
 
 findTwo2020(inputArray);
 
+/*
+They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to 2020?
+*/
+
 function findThree2020(arr) {
   for (let n of arr) {
     for (let i of arr) {
--- d-2/index.js
+++ d-2/index.js
@@ -48,9 +48,20 @@ function validPassword(passObj) {
 
 let validPasswords = 0;
 
-// for (password of passwords) {
-//   validPassword(password) ? validPasswords++ : null;
-// }
+for (password of passwords) {
+  validPassword(password) ? validPasswords++ : null;
+}
+
+/*
+  Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+  Given the same example list from above:
+
+  1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+  1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+  2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+  How many passwords are valid according to the new interpretation of the policies?
+*/
 
 function validPasswordPositions(password) {
   let firstInstance = password.password[password.min - 1];
@@ -61,8 +72,8 @@ function validPasswordPositions(password) {
   );
 }
 
-for (password of passwords) {
-  validPasswordPositions(password) ? validPasswords++ : null;
-}
+let validPositionPasswords = 0;
 
-console.log(validPasswords);
+for (password of passwords) {
+  validPasswordPositions(password) ? validPositionPasswords++ : null;
+}
