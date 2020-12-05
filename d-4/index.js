469fa6c57330ccb3466f1b6b1f37cb7085ddaf43
Emily Harber <emily.a.harber@gmail.com>
2020-12-04 21:37:24 -0500

    Finishes day 4 part 1

---
+++ d-4/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+let rawData = fs.readFileSync('d-4/input.txt', { encoding: 'utf-8' });
+
+let passports = rawData.split('\n\n');
+
+let requiredFields = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:'];
+
+let validCount = 0;
+
+for (passport of passports) {
+  let trimmedPassport = passport.replace(/(\n|\r)/g, '');
+  let valid = true;
+  for (field of requiredFields) {
+    if (!trimmedPassport.includes(field)) {
+      valid = false;
+      break;
+    }
+  }
+  valid ? validCount++ : null;
+}
+
+console.log(validCount);
