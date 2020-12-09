17fef57533efe6a544b807f30e3e1f5ec1d90887
Emily Harber <emily.a.harber@gmail.com>
2020-12-08 20:34:49 -0500

    Fix messed up day 5 file

--- d-5/index.js
+++ d-5/index.js
@@ -6,9 +6,7 @@ let passes = rawFile.split('\n');
 
 /* 
   The first 7 characters will either be F or B; these specify exactly one of the 128 rows on the plane (numbered 0 through 127). Each letter tells you which half of a region the given seat is in. Start with the whole list of rows; the first letter indicates whether the seat is in the front (0 through 63) or the back (64 through 127). The next letter indicates which half of that region the seat is in, and so on until you're left with exactly one row.
-
   For example, consider just the first seven characters of FBFBBFFRLR:
-
   Start by considering the whole range, rows 0 through 127.
   F means to take the lower half, keeping rows 0 through 63.
   B means to take the upper half, keeping rows 32 through 63.
@@ -18,21 +16,17 @@ let passes = rawFile.split('\n');
   F keeps rows 44 through 45.
   The final F keeps the lower of the two, row 44.
   The last three characters will be either L or R; these specify exactly one of the 8 columns of seats on the plane (numbered 0 through 7). The same process as above proceeds again, this time with only three steps. L means to keep the lower half, while R means to keep the upper half.
-
   For example, consider just the last 3 characters of FBFBBFFRLR:
-
   Start by considering the whole range, columns 0 through 7.
   R means to take the upper half, keeping columns 4 through 7.
   L means to take the lower half, keeping columns 4 through 5.
   The final R keeps the upper of the two, column 5.
   So, decoding FBFBBFFRLR reveals that it is the seat at row 44, column 5.
-
   Every seat also has a unique seat ID: multiply the row by 8, then add the column. In this example, the seat has ID 44 * 8 + 5 = 357.
-
   return the highest seat ID
 */
 
-let highestID = 0;
+let allIDs = [];
 
 for (pass of passes) {
   let low = 0;
@@ -65,9 +59,22 @@ for (pass of passes) {
 
   let id = row * 8 + seat;
 
-  console.log(id);
-
-  highestID = highestID < id ? id : highestID;
+  allIDs.push(id);
 }
 
-console.log('highestID: ', highestID);
+let sortedIDs = allIDs.sort((a, b) => a - b);
+
+let prevID = sortedIDs[0];
+let missingID = 0;
+
+for (let i = 1; i < sortedIDs.length; i++) {
+  if (sortedIDs[i] !== prevID + 1) {
+    missingID = sortedIDs[i] - 1;
+    break;
+  } else {
+    prevID = sortedIDs[i];
+  }
+}
+
+console.log('highestID: ', sortedIDs[sortedIDs.length - 1]);
+console.log('my pass ID: ', missingID);
